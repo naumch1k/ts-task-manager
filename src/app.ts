a998cf3c19a92f91eb3067a7e8444136133c12d5
Irina Naumchik <irina.naumchik2012@yandex.ru>
2022-08-11 16:56:53 -0500

    feat: gather user input, clear form after form submit

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ class TaskForm {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('task-form') as HTMLTemplateElement;
@@ -11,7 +13,48 @@ class TaskForm {
         this.element = importedNode.firstElementChild as HTMLFormElement;
         this.element.id = 'user-input';
 
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+
+        this.configure();
         this.attach();
+    }
+
+    private getUserInput(): [string, string] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+
+        // TODO: improve validation
+        if (
+            enteredTitle.trim().length === 0 ||
+            enteredDescription.trim().length === 0
+        ) {
+            alert('Invalid input, please try again');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription];
+        }
+    }
+
+    private clearForm() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        const userInput = this.getUserInput();
+
+        if (Array.isArray(userInput)) {
+            const [title, description] = userInput;
+            console.log(title, description);
+            this.clearForm();
+        }
+    }
+
+    private configure() {
+        // TODO: create autobind decorator
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
 
     private attach() {
